2b1b5c6148e9d48925fe1fe7e6d81e698df492b4
seanaleid <seanaleid@gmail.com>
2020-04-06 13:26:41 -0400

    Write a .GET endpoint at /api/hubs
      * Adds an array and returns it when the user pings /api/hubs

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ const express = require('express'); // similar ot above, but works in all node v
 const server = express();
 
 // endpoints
+server.get('/', (req, res) => {
+    res.json({ api: 'running.....'});
+})
+
+server.get('/api/hubs/', (req, res) => {
+    const hubs = [
+        {
+            id: 1,
+            name: 'web28',
+            lessonId: 1,
+            cohort: 'web28'
+        },
+        {
+            id: 2,
+            name: 'web28',
+            lessonId: 1,
+            cohort: 'web28'
+        },
+        {
+            id: 3,
+            name: 'web28',
+            lessonId: 1,
+            cohort: 'web28'
+        },
+    ]
+    
+    res.json(hubs);
+})
 
 const port = 5000; // the server is running on http://localhost:5000/
 server.listen(port, () => console.log(`\n === api running on port ${port} ===\n`))
